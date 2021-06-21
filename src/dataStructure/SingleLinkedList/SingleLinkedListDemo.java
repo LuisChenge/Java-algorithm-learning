b6456cf819ee06ac2740eb49bb9fc468ddf3042f
luischenge <luischenge@foxmail.com>
2021-06-21 22:19:00 +0800

    更新单链表代码

--- src/dataStructure/SingleLinkedList/SingleLinkedListDemo.java
+++ src/dataStructure/SingleLinkedList/SingleLinkedListDemo.java
@@ -2,75 +2,92 @@ package dataStructure.SingleLinkedList;
 
 public class SingleLinkedListDemo {
     public static void main(String[] args) {
-        HeroNode hero1 = new HeroNode(1, "aaa");
-        HeroNode hero2 = new HeroNode(2, "bbb");
-        HeroNode hero3 = new HeroNode(3, "ccc");
-        HeroNode hero4 = new HeroNode(4, "ddd");
-        HeroNode hero5 = new HeroNode(5, "eee");
-
-        SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.add(hero1);
-        singleLinkedList.add(hero2);
-        singleLinkedList.add(hero3);
-        singleLinkedList.add(hero4);
-        singleLinkedList.add(hero5);
-        singleLinkedList.list();
-    }
-}
-
-class SingleLinkedList {
-    // 先初始化一个头节点。
-    private final HeroNode head = new HeroNode(0, "");
-
-    public void add(HeroNode heroNode) {
-        HeroNode temp = head;
-        while (true) {
-            if (temp.next == null) {
-                break;
-            }
-            temp = temp.next;
-        }
-        temp.next = heroNode;
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.addAtHead(1);
+        linkedList.addAtTail(3);
+        linkedList.addAtIndex(1, 2);   //链表变为1-> 2-> 3
+        System.out.println(linkedList.get(1));            //返回2
+        linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+        System.out.println(linkedList.get(1));            //返回3
     }
 
-    public void list() {
-        if (head.next == null) {
-            System.out.println("链表为空");
-            return;
-        }
-        HeroNode temp = head.next;
-        while (true) {
-            if (temp == null) {
-                break;
-            }
-            System.out.println(temp);
-            // 将 temp 后移
-            temp = temp.next;
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
         }
     }
-}
 
+    static class MyLinkedList {
+        int size;
+        ListNode head;  // sentinel node as pseudo-head
 
-class HeroNode {
-    public int no;                  // 序列号
-    public String name;             // 名字
-    public HeroNode next;
+        public MyLinkedList() {
+            size = 0;
+            head = new ListNode(0);
+        }
 
-    //  构造方法
-    public HeroNode(int no, String name) {
-        this.no = no;
-        this.name = name;
+        // Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        public int get(int index) {
+            // if index is invalid
+            if (index < 0 || index >= size) return -1;
+
+            ListNode curr = head;
+            // index steps needed
+            // to move from sentinel node to wanted index
+            for (int i = 0; i < index + 1; ++i) curr = curr.next;
+            return curr.val;
+        }
+
+        //  Add a node of value val before the first element of the linked list.
+        //  After the insertion, the new node will be the first node of the linked list.
+        public void addAtHead(int val) {
+            addAtIndex(0, val);
+        }
+
+        //  Append a node of value val to the last element of the linked list. */
+        public void addAtTail(int val) {
+            addAtIndex(size, val);
+        }
+
+        //  Add a node of value val before the index-th node in the linked list.
+        //  If index equals to the length of linked list, the node will be appended to the end of linked list.
+        //  If index is greater than the length, the node will not be inserted. */
+        public void addAtIndex(int index, int val) {
+            // If index is greater than the length,
+            // the node will not be inserted.
+            if (index > size) return;
+
+            // [so weird] If index is negative,
+            // the node will be inserted at the head of the list.
+            if (index < 0) index = 0;
+
+            ++size;
+            // find predecessor of the node to be added
+            ListNode pred = head;
+            for (int i = 0; i < index; ++i) pred = pred.next;
+
+            // node to be added
+            ListNode toAdd = new ListNode(val);
+            // insertion itself
+            toAdd.next = pred.next;
+            pred.next = toAdd;
+        }
+
+        //  Delete the index-th node in the linked list, if the index is valid.
+        public void deleteAtIndex(int index) {
+            // if the index is invalid, do nothing
+            if (index < 0 || index >= size) return;
+
+            size--;
+            // find predecessor of the node to be deleted
+            ListNode pred = head;
+            for (int i = 0; i < index; ++i) pred = pred.next;
+
+            // delete pred.next
+            pred.next = pred.next.next;
+        }
     }
-
-
-    // 重写 toString()
-    @Override
-    public String toString() {
-        return "HeroNode{" +
-                "no=" + no +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-
 }
